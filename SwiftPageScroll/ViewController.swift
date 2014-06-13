f127b8b7029f392003b7b24cabc0582d4dd9b4c7
DehengXu <dehengxu@outlook.com>
2014-06-13 16:11:31 +0800

    Init commit

--- SwiftPageScroll/ViewController.swift
+++ SwiftPageScroll/ViewController.swift
@@ -8,11 +8,20 @@
 
 import UIKit
 
+
 class ViewController: UIViewController {
-                            
+    
+    var scrollView: UIScrollView?
+    let count:UInt = 4
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        initSubviews()
+        
+        view.addSubview(scrollView);
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +29,26 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    func initSubviews() {
+        var fm: CGRect = UIScreen.mainScreen().bounds
+        
+        var gaps :Float = 20
+        
+        self.scrollView = UIScrollView(frame:CGRectMake(0, 0, fm.size.width + gaps, fm.size.height))
+        self.scrollView!.contentSize =   CGSizeMake(self.scrollView!.frame.size.width * Float(count), fm.size.height)
+        self.scrollView!.backgroundColor = UIColor.blackColor()
+        self.scrollView!.pagingEnabled = true
+        self.scrollView!.bounces = false
+        
+        for i in 0..count {
+            var offset = i * 320
+            println("offset :\(offset)")
+            var v :UIView =  UIView(frame:  CGRectMake(Float(i) * self.scrollView!.frame.size.width, 0, fm.size.width, fm.size.height))
+            v.backgroundColor = UIColor.lightGrayColor()
+            self.scrollView?.addSubview(v);
+        }
+    }
 
 }
 
